cfa88a191b33e0aac5e73c7b8087bfd08e6451c9
Adrian Górski <agrski06@gmail.com>
2022-06-19 15:15:37 +0200

    added check for null message in MessageService

--- src/main/java/com/adrian/gorski/discordBot/api/MessageService.java
+++ src/main/java/com/adrian/gorski/discordBot/api/MessageService.java
@@ -2,7 +2,8 @@ package com.adrian.gorski.discordBot.api;
 
 import com.adrian.gorski.discordBot.config.Bot;
 import lombok.RequiredArgsConstructor;
-import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.TextChannel;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,7 +13,13 @@ public class MessageService {
     private final Bot bot;
 
     public void sendMessage(String message) {
-        bot.getJda().getGuildById("978261544037138533").getTextChannelById("978261544037138536").sendMessage(message).queue();
+        Guild guild = bot.getJda().getGuildById("978261544037138533");
+        if (guild != null) {
+            TextChannel channel = guild.getTextChannelById("978261544037138536");
+            if (channel != null) {
+                channel.sendMessage(message).queue();
+            }
+        }
     }
 
 }
